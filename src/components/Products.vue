c75012b515cc8092027a4c14fa8a09763e8d25fa
Allan Chávez <allancss2009@hotmail.com>
2022-10-11 14:16:53 -0600

    feat: vue project for shipping website api

---
+++ src/components/Products.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="products">Hola
+     <div v-for="(product, index) in products" :key="product.id">
+      <p>The new product is: {{product.name}}</p>
+     </div>
+    </div>
+   </template>
+
+<script>
+
+export default {  
+ name: 'Products',
+ data() {
+  return {
+    products: []
+  }
+ },
+ created() {
+  axios.get('localhost:3000/products') 
+  .then(response => {
+   this.products = response.data
+  })
+ .catch(e => {
+  this.error.push(e)
+  })
+ },
+}
+</script>
